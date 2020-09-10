56da3a16ede7d415cb1ced42418f062f0b678599
jholmstadt <john.holmstadt@nexteraanalytics.com>
2020-09-10 11:22:00 -0500

    ISSUE 33 - Check to see if the current number of minutes to the meeting is equal to one of the reminder_at_tminus_minutes array values.

--- express-server/jobService.js
+++ express-server/jobService.js
@@ -154,10 +154,10 @@ async function sendReminderIfNecessary(events, reminders) {
         //const tminusSeconds = (firstEvent.startDate - currentTime) / 1000;
         const tminusMinutes = Math.round((firstEvent.startDate - currentTime) / 1000 / 60);
         console.log(`T-minus ${tminusMinutes} minute(s)`);
-        if (reminders.indexOf(tminusMinutes) === -1) {
-            console.log(`Nag skipped because ${tminusMinutes} not found in ${reminders}`);
-            return;
-        }
+//        if (reminders.indexOf(tminusMinutes) !== -1) {
+//            console.log(`Nag skipped because ${tminusMinutes} not found in ${reminders}`);
+//            return;
+//        }
         if (tminusMinutes > 1) {
             var timeUnit = "minutes";
         } else {
