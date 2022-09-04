547233c4c4e74fc600955704cdb9b837dc4b6a03
Jack Stobbe <jjstobbe@gmail.com>
2022-09-04 16:45:49 -0500

    Seems to work 👍

--- netlify/functions/cron-update-slack-status.ts
+++ netlify/functions/cron-update-slack-status.ts
@@ -1,8 +1,8 @@
 import { Handler, schedule } from "@netlify/functions";
 import { runSlackStatusUpdaterJob } from "../../services/jobService";
 
-// Runs every two minutes from 7:00 AM - 5:00 PM on every weekday (+Sunday for testing right now)
-const handler: Handler = schedule('*/2 7-17 * * 0-5', async (event, context) => {
+// Runs every two minutes from 7:00 AM - 5:00 PM on every weekday
+const handler: Handler = schedule('*/2 7-17 * * 1-5', async (event, context) => {
     await runSlackStatusUpdaterJob();
 
     return {
